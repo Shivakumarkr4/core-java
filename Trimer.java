0aabc25399854642ddb177ed51588babccee4f70
Shivakumarkr4 <shivakumarkr.xworkz@gmail.com>
2022-06-21 09:38:25 +0530

    Committing trimmer file

---
+++ Trimer.java
@@ -0,0 +1,14 @@
+class Facewash{
+static String brand = "Aclenak";
+static int price = 240;
+static String colur= "white";
+static int quantity= 200;		
+public static void main(String a[])
+{
+System.out.println("the facewash brand is "+brand);
+System.out.println("the facewash  price is "+price);
+System.out.println("the facewash colur is "+colur);
+System.out.println("the facewash quantity is "+ quantity);
+}
+
+}
